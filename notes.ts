b47273fb795a0dbf4498e807344d16037c53a084
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-06-14 12:43:02 +0530

    Initial commit from Create Next App

---
+++ notes.ts
@@ -0,0 +1,11 @@
+// ### ALL Prisma Notes will be written here :
+
+// #Starting with Prisma:
+// 1.) Prisma install : "npm install prisma @prisma/client" --> install Prisma and its client library
+//    - Prisma is an ORM (Object Relational Mapping) tool that helps in database management
+
+// 2.) Prisma init : "npx prisma init" --> Setup krne ke liye .env file mein DATABASE_URL set karna padega
+//    - This command initializes Prisma in your project, creating a `prisma` folder with a `schema.prisma` file
+//    - The `schema.prisma` file is where you define your data model and database
+
+// 3.)
